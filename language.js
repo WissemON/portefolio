aa90ae210034de57c48ea36793d0e2c2008fa801
Wissem <105933942+WissemON@users.noreply.github.com>
2023-03-29 15:57:15 +0200

    p'tit responsif + début script lang

---
+++ language.js
@@ -0,0 +1,9 @@
+let frenchFlag = document.querySelector('#fr_flag');
+let englishFlag = document.querySelector('#en_flag');
+let en = document.querySelector('.english');
+let fr = document.querySelector('.french');
+let lightSelect = document.querySelector('.light_select');
+
+function ChangeToEnglish() { 
+      frenchFlag.classList.toggle("light")
+}
